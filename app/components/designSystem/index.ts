89a569fd67aae0ab86714b8bbd073a176405ea72
Mike DiGioia <mdigioia@aps.org>
2022-08-10 23:35:20 -0400

    feat: :art: moved CounterButton.tsx to CounterButton.client.tsx
    To better describe the components target.  Need to figure out an ssr story for these components

--- app/components/designSystem/index.ts
+++ app/components/designSystem/index.ts
@@ -4,6 +4,4 @@
 // SSR may be resolved soon and it can be ssr'd
 // https://github.com/lit/lit/issues/2524
 import lazy from "next/dynamic"
-export const CounterButton = lazy(() => import("./CounterButton"), {
-  ssr: false,
-});
+export const CounterButton = lazy(() => import("./CounterButton.client"), { ssr: false });
